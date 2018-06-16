d40ccb0cb37f28198b75b5484792a0cf4bc6b90a
Mahendra Yadav <mahendra.k12@gmail.com>
2018-06-16 19:06:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,17 @@ function nowServing(katzDeliLine){
     return `Currently serving ${katzDeliLine.shift()}.`;
   }
 }
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty.";
+  }
+  else{
+    var newString = "The line is currently: ";
+    var array = new Array();
+    for (var i=0; i<katzDeliLine.length; i++){
+      array.push(`${i+1}. ${katzDeliLine[i]}`);
+    }
+    newString += string.join(", ", array);
+    return newString;
+  } 
+}
